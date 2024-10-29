7f1029b4834eec1ade7836dcedf8b981e2cab41f
parukrev <xavierrevian@gmail.com>
2024-10-29 16:30:13 +0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 
 const App = () => {
   const [name, setName] = useState('');
-  const [phone, setPhone] = useState(0);
+  const [phone, setPhone] = useState('');
   const [notification, setNotification] = useState('');
+  const [phoneBook, setPhoneBook] = useState([]);
 
   const addNewNumber = async () => {
     try {
       await Axios.post('http://localhost:8080/add-phone', {name, phone});
       setNotification('Number added successfully');
       setName('');
-      setPhone(0);
+      setPhone('');
+      fetchPhoneBook();
     } catch (error) {
       setNotification('Failed to add number');
     }
   }
+
+  const fetchPhoneBook = async () => {
+    try {
+      const response = await Axios.get('http://localhost:8080/get-phone');
+      console.log(response.data);
+      // setPhoneBook(response.data.phoneNumbers);
+      setPhoneBook(Array.isArray(response.data.phoneNumbers) ? response.data : []);
+    } catch (error) {
+      console.error('Failed to fetch phone book: ', error);
+      setPhoneBook([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhoneBook();
+  }, []);
 
   return (
     <div className='container'>
@@ -28,6 +46,15 @@ const App = () => {
       <button onClick={addNewNumber}>Add New Number</button>
 
       {notification && <p>{notification}</p>}
+
+      <h1>PhoneBook List</h1>
+      <ul>
+        {Array.isArray(phoneBook) && phoneBook.length > 0 ? (
+          phoneBook.map((phone) => (
+            
+          )
+        )}
+      </ul>
     </div>
   )
 }
